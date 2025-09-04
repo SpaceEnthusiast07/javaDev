7d648459b8b10e17dc8b3c4a8586a1663bf1fd85
Cameron <null@gmail.com>
2025-09-04 20:41:29 +0100

    Standard file save

---
+++ SwingPractice/src/MultiplePages.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class MultiplePages extends JFrame implements ActionListener {
+    public MultiplePages() {
+        super("Java Swing with Multiple Pages"); // Sets title of window
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1920, 1080);
+        setVisible(true);
+    }
+    
+    public static void main (String[] args) {
+        new MultiplePages();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
