48bc021f0f3958bb2cd790d28566e3b1838ea534
Cameron <chrussellpredator07@gmail.com>
2025-09-05 17:02:30 +0100

    Standard file save

--- SwingPractice/src/MultiplePages.java
+++ SwingPractice/src/MultiplePages.java
@@ -2,15 +2,25 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class MultiplePages extends JFrame implements ActionListener {
+    // Creates the two main panels
+    JPanel panel1;
+    JPanel panel2;
+    
     public MultiplePages() {
-        super("Java Swing with Multiple Pages"); // Sets title of window
+        // Accesses the constructor of JFrame and sets window title
+        super("Java Swing with Multiple Pages");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1920, 1080);
         setVisible(true);
+        // Adds the two panels to the JFrame
+        add("North", panel1);
+        add("South", panel2);
     }
     
     public static void main (String[] args) {
-        new MultiplePages();
+        MultiplePages mainWindow = new MultiplePages();
+        mainWindow.add("centre", mainWindow.panel1);
+        
     }
 
     public void actionPerformed(ActionEvent e) {
