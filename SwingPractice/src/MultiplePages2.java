3ebe5925984df4a42f78e46717e82316bc455b02
Cameron <chrussellpredator07@gmail.com>
2025-09-06 20:12:43 +0100

    Standard file save

--- SwingPractice/src/MultiplePages2.java
+++ SwingPractice/src/MultiplePages2.java
@@ -4,6 +4,18 @@ import java.awt.*;
 
 public class MultiplePages2 {
     public static void main (String[] args) {
+
+    }
+}
+
+class GUI extends JFrame implements ActionListener {
+    public GUI (String title, int width, int height) {
+        super(title);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(width, height);
+    }
+
+    public void actionPerformed(ActionEvent e) {
         
     }
 }
