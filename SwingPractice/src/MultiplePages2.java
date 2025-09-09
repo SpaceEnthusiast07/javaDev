d240216b9b2941ed45ff482df93b497dfe922970
Cameron <chrussellpredator07@gmail.com>
2025-09-09 14:28:56 +0100

    Standard file save

--- SwingPractice/src/MultiplePages2.java
+++ SwingPractice/src/MultiplePages2.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.awt.GridLayout;
 import java.awt.Color.*;
 
 public class MultiplePages2 implements ActionListener {
@@ -10,10 +11,11 @@ public class MultiplePages2 implements ActionListener {
     
     public static void main (String[] args) {
         mainWindow = new JFrame("Multiple Pages Example"); // Set title of window
-        mainWindow.setSize(400, 400); // Set dimensions
+        mainWindow.setSize(1280, 720); // Set dimensions
         mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exit program when window is closed
         
         mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(1, 2)); // Set layout to grid with 1 row and 2 columns
         mainWindow.add(mainPanel); // Add panel to window
 
         mainWindow.setVisible(true); // Make window visible
