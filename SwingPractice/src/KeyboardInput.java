58f08ccc545d7d2bd9937e257224f4c1dbcb1294
Cameron <chrussellpredator07@gmail.com>
2025-10-08 16:24:50 +0100

    Standard file save

--- SwingPractice/src/KeyboardInput.java
+++ SwingPractice/src/KeyboardInput.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.*;
@@ -12,44 +14,60 @@ import java.awt.event.*;
 class KeyboardInput extends JFrame implements KeyListener{
     // Define the main panel
     JPanel mainPanel;
+
     // Define the labels
     JLabel keyPressedlabel;
     JLabel keyTypedlabel;
     JLabel keyReleasedlabel;
     JLabel label;
+
     // Define the key variable
     String key;
     
     
     // Constructor
     public KeyboardInput() {
-        // Set window title
+        // Set window title and other attributes
         super("Keyboard Input Test");
         setSize(600, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         addKeyListener(this);
 
+
         // Initialise the main panel
         mainPanel = new JPanel();
         mainPanel.setOpaque(true);
+
         // Add the panel to the mainWindow
         add(mainPanel);
 
-        // Initialise the labels
+
+        // Initialise the label's text, font style and foreground
         keyPressedlabel = new JLabel("Press any key!");
-        keyPressedlabel.setFont(new Font("Arial", Font.PLAIN, 24));
+        keyPressedlabel.setFont(new Font("Consolas", Font.PLAIN, 24));
+        keyPressedlabel.setForeground(new Color(123,193,175));
+        keyPressedlabel.setBorder(new LineBorder(Color.BLACK, 1, false));
+
         keyTypedlabel = new JLabel("Press any key!");
-        keyTypedlabel.setFont(new Font("Arial", Font.PLAIN, 24));
+        keyTypedlabel.setFont(new Font("Consolas", Font.PLAIN, 24));
+        keyTypedlabel.setForeground(new Color(164, 72, 100));
+        keyTypedlabel.setBorder(new LineBorder(Color.BLACK, 1, false));
+
         keyReleasedlabel = new JLabel("Press any key!");
-        keyReleasedlabel.setFont(new Font("Arial", Font.PLAIN, 24));
+        keyReleasedlabel.setFont(new Font("Consolas", Font.PLAIN, 24));
+        keyReleasedlabel.setForeground(new Color(156, 102, 76));
+        keyReleasedlabel.setBorder(new LineBorder(Color.BLACK, 1, false));
+
         label = new JLabel("Aaaarrrggghhhh!!!!!");
-        label.setFont(new Font("Arial", Font.PLAIN, 48));
+        label.setFont(new Font("Consolas", Font.PLAIN, 48));
+
 
         // Add them to the mainPanel
         mainPanel.add(keyPressedlabel);
         mainPanel.add(keyTypedlabel);
         mainPanel.add(keyReleasedlabel);
         mainPanel.add(label);
+
 
         setVisible(true);
     }
@@ -67,6 +85,7 @@ class KeyboardInput extends JFrame implements KeyListener{
         // Get the key pressed
         key = event.getKeyText(event.getKeyCode());
         
+        // Set the text of the label to include the key pressed
         keyPressedlabel.setText("keyPressed: " + key);
         label.setText(key);
 
@@ -83,7 +102,7 @@ class KeyboardInput extends JFrame implements KeyListener{
 
     // Called after the key is pressed
     public void keyTyped(KeyEvent event){
-        keyTypedlabel.setText("Key Typed: " + event.getKeyChar());
+        keyTypedlabel.setText("keyTyped: " + event.getKeyChar());
     }
 
     // Called when the key is released
